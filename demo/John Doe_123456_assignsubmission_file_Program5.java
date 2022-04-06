01a2ba86aa9bc5390c8c80b0678600aa50ddf5e0
skramm <sebastien.kramm@univ-rouen.fr>
2022-04-06 16:37:35 +0200

    demo in progress

---
+++ demo/John Doe_123456_assignsubmission_file_Program5.java
@@ -0,0 +1,22 @@
+public class Program5
+{
+	public static void main( String[] args )
+	{
+		if( args.length == 0 )
+		{
+			System.out.println("missings args");
+			System.exit(1);
+		}
+		double v = Double.parseDouble(args[1]);
+		
+		if(args[0].equals("P") )
+			System.out.println(v*v);
+		else
+		{
+			if(args[0].equals("S") )
+				System.out.println(v+v);
+			else
+				System.out.println("invalid arguments");
+		 }
+	}
+}
